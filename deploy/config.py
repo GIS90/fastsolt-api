11abace472217a63d1cee1ee276ff96de086d5de
mingliang.gao <gaoming971366@163.com>
2025-12-15 21:45:53 +0800

    refactor：新增try...except机制

--- deploy/config.py
+++ deploy/config.py
@@ -52,15 +52,13 @@ if (not etc_file.exists()
         or not etc_file.is_file()):
     printer_error(content=f"配置文件[{env_file}]不存在，系统自动退出......", hr=True)
     sys.exit(1)
-else:
-    printer_info(content=f"当前环境：{env}，配置文件：{etc_file}", hr=True)
-
 
 try:
     with open(etc_file, "rb") as f:
         data = tomllib.load(f)
+    printer_info(content=f"当前环境：{env}，配置文件：{etc_file}", hr=True)
 except:
-    printer_info(content=f"配置文件解析失败，请检查配置：{env_file}", hr=True)
+    printer_error(content=f"配置文件解析失败，请检查配置：{env_file}", hr=True)
     sys.exit(1)
 
 
